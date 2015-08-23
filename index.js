172396a5c2287772385cef97956a2ac9cd9bcaeb
Ashay Naik <ashaydnaik@hotmail.com>
2015-08-23 10:45:16 +1000

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+//app.get('/', function(request, response) {
+//  response.render('pages/index');
+//});
+
+app.post('/filterService/', function(req, res, next) {
+  var obj = req.body;
+  obj["age"] = 25;
+  res.send(obj);
 });
 
 app.listen(app.get('port'), function() {
